8a1ede94c0379c8ff30f7c8f3152813c467ae3cd
Artem <a.ku666@vk.com>
2023-11-06 13:56:27 +0200

    miniRepair

--- src/App.js
+++ src/App.js
@@ -10,20 +10,22 @@ const item = {
   currency: "£",
 };
 
-const ShopItemFunc = () => {
+const ShopItemFunc = (props) => {
   return (
     <div className="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div className="description">{item.descriptionFull}</div>
+      <h2>{props.item.brand}</h2>
+      <h1>{props.item.title}</h1>
+      <h3>{props.item.description}</h3>
+      <div className="description">{props.item.descriptionFull}</div>
 
       <div className="highlight-window mobile">
         <div className="highlight-overlay"></div>
       </div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">{item.currency + item.price}</div>
+        <div className="price">
+          {props.item.currency + props.item.price + ".00"}
+        </div>
         <button>Добавить в корзину</button>
       </div>
     </div>
